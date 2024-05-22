282cea28de77b4f61dbf478352ce7be1d5cab533
JayWok <jaydennlocke@gmail.com>
2024-05-22 14:37:28 -0400

    Fixed nav menu light/dark mode

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("modeToggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
   document.querySelector("header").classList.toggle("dark-mode");
+  document.querySelector("nav").classList.toggle("dark-mode");
   var sections = document.querySelectorAll("section");
   sections.forEach(function (section) {
     section.classList.toggle("dark-mode");
